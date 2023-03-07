146b3bfd8aa0ce23fdb52ed5514efc31912fbcd0
Quetzalcoatl <63520792+Qu3tzalcoatl@users.noreply.github.com>
2023-03-07 11:25:32 -0800

    Batch files added for Swinir HDR training

---
+++ fake_train_swinir.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=SwinIR
+#SBATCH --nodes 1
+#SBATCH --tasks-per-node=1
+#SBATCH --account=def-panos
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=00:15:00
+
+
+module load python/3.10
+
+module load  StdEnv/2020  cuda cudnn
+module load gcc opencv
+
+nvidia-smi
+
+
+source  ~/ENV1/bin/activate
+
+cd ~
+
+echo "Testing..."
+
+
+python ~/KAIR/main_train_psnr.py --opt ~/KAIR/options/swinir/train_swinir_hdr_v2.json
+
+
+
