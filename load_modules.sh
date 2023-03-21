ddf8e8e37a704673a67ff3e065bad5f7d307e40f
Spencer Lee <slee67@narval3.narval.calcul.quebec>
2023-03-20 20:28:24 -0400

    Added the ENV virtual environment to the KAIR folder

---
+++ load_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+module load python/3.10
+module load StdEnv/2020 cuda cudnn
+module load gcc opencv
+virtualenv --no-download ENV
+source ENV/bin/activate
+pip install --no-index --upgrade pip
+pip install numpy --no-index
+cd KAIR
+pip install -r requirement.txt
